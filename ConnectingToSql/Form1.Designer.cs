491e326bca013ae2c94b0aa938f5653e3f1386fa
Victor <vcl@telia.com>
2022-07-25 19:58:59 +0200

    rest api grpbox3

--- ConnectingToSql/Form1.Designer.cs
+++ ConnectingToSql/Form1.Designer.cs
@@ -32,24 +32,32 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtContact = new System.Windows.Forms.TextBox();
-            this.txtAdress = new System.Windows.Forms.TextBox();
-            this.cmbClass = new System.Windows.Forms.ComboBox();
-            this.btnSave = new System.Windows.Forms.Button();
             this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cmbClass = new System.Windows.Forms.ComboBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrlRaw = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtDataResp = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(33, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(41, 46);
             this.button1.TabIndex = 0;
@@ -60,7 +68,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(33, 70);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(166, 161);
@@ -71,9 +79,9 @@
             // 
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(109, 231);
+            this.groupBox1.Location = new System.Drawing.Point(908, 354);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(74, 100);
+            this.groupBox1.Size = new System.Drawing.Size(36, 66);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -90,71 +98,32 @@
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(503, 256);
+            this.groupBox2.Location = new System.Drawing.Point(879, 189);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(143, 139);
+            this.groupBox2.Size = new System.Drawing.Size(102, 66);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
-            // label1
+            // btnConnect
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Student Name";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.btnConnect.Location = new System.Drawing.Point(304, 40);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
-            // label2
+            // btnSave
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Student Class";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 143);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Student Adress";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 190);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Student Contact";
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(144, 37);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(143, 20);
-            this.txtName.TabIndex = 4;
-            // 
-            // txtContact
-            // 
-            this.txtContact.Location = new System.Drawing.Point(144, 187);
-            this.txtContact.Name = "txtContact";
-            this.txtContact.Size = new System.Drawing.Size(143, 20);
-            this.txtContact.TabIndex = 5;
-            // 
-            // txtAdress
-            // 
-            this.txtAdress.Location = new System.Drawing.Point(144, 137);
-            this.txtAdress.Multiline = true;
-            this.txtAdress.Name = "txtAdress";
-            this.txtAdress.Size = new System.Drawing.Size(143, 44);
-            this.txtAdress.TabIndex = 6;
+            this.btnSave.Location = new System.Drawing.Point(304, 77);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // cmbClass
             // 
@@ -176,45 +145,159 @@
             "Boromir",
             "Radagast",
             "Aragorn"});
-            this.cmbClass.Location = new System.Drawing.Point(144, 89);
+            this.cmbClass.Location = new System.Drawing.Point(144, 69);
             this.cmbClass.Name = "cmbClass";
             this.cmbClass.Size = new System.Drawing.Size(143, 21);
             this.cmbClass.TabIndex = 7;
             // 
-            // btnSave
+            // txtAdress
             // 
-            this.btnSave.Location = new System.Drawing.Point(180, 267);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 8;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.txtAdress.Location = new System.Drawing.Point(144, 97);
+            this.txtAdress.Multiline = true;
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(143, 44);
+            this.txtAdress.TabIndex = 6;
             // 
-            // btnConnect
+            // txtContact
             // 
-            this.btnConnect.Location = new System.Drawing.Point(180, 316);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
-            this.btnConnect.TabIndex = 9;
-            this.btnConnect.Text = "Connect";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            this.txtContact.Location = new System.Drawing.Point(144, 147);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(143, 20);
+            this.txtContact.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(144, 40);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(143, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Student Contact";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Adress";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Class";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUrlRaw
+            // 
+            this.txtUrlRaw.Location = new System.Drawing.Point(80, 37);
+            this.txtUrlRaw.Name = "txtUrlRaw";
+            this.txtUrlRaw.Size = new System.Drawing.Size(197, 20);
+            this.txtUrlRaw.TabIndex = 10;
+            this.txtUrlRaw.TextChanged += new System.EventHandler(this.urlTxt);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Paste URL";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Ok!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_OkUrl);
+            // 
+            // txtDataResp
+            // 
+            this.txtDataResp.Location = new System.Drawing.Point(18, 81);
+            this.txtDataResp.Multiline = true;
+            this.txtDataResp.Name = "txtDataResp";
+            this.txtDataResp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDataResp.Size = new System.Drawing.Size(340, 240);
+            this.txtDataResp.TabIndex = 13;
+            this.txtDataResp.TextChanged += new System.EventHandler(this.txtDataResp_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.txtDataResp);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.txtUrlRaw);
+            this.groupBox3.Location = new System.Drawing.Point(251, 35);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(576, 383);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(391, 81);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Connect to SQL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_ConnectSql3);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(391, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Save to SQL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_saveToSql3);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 432);
+            this.ClientSize = new System.Drawing.Size(1029, 432);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -235,6 +318,13 @@
         private System.Windows.Forms.TextBox txtAdress;
         private System.Windows.Forms.TextBox txtContact;
         private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUrlRaw;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtDataResp;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ConnectingToSql/Form1.cs
+++ ConnectingToSql/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using ConnectingToSql.connectToSql;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace ConnectingToSql
 {
@@ -17,11 +20,11 @@ namespace ConnectingToSql
         connectSQLServer conSqlServ = new connectSQLServer();
         SqlConnection sqlcon = new SqlConnection();
         SqlCommand cmd = new SqlCommand();
-
+        JObject jsonObjectSave;
         public Form1()
         {
             InitializeComponent();
-           // sqlcon = conSqlServ.GetConnection();
+            // sqlcon = conSqlServ.GetConnection();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -35,19 +38,19 @@ namespace ConnectingToSql
 
             while (reader.Read())
             {
-                string output = "Output = " + reader.GetValue(0) + "-" + reader.GetValue(1) + "-" + reader.GetValue(2)+ "-" + reader.GetValue(3)+ "-" + 
+                string output = "Output = " + reader.GetValue(0) + "-" + reader.GetValue(1) + "-" + reader.GetValue(2) + "-" + reader.GetValue(3) + "-" +
                     reader.GetValue(4);
-                textBox1.Text += reader.GetValue(0).ToString()+ " " + reader.GetValue(1).ToString()+ " " + reader.GetValue(2).ToString()+ " " + 
+                textBox1.Text += reader.GetValue(0).ToString() + " " + reader.GetValue(1).ToString() + " " + reader.GetValue(2).ToString() + " " +
                     reader.GetValue(3).ToString() + " " + reader.GetValue(4).ToString() + Environment.NewLine;
                 // Hämtar data från SQL och sedan visar det på textboxen
             }
 
-            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -65,10 +68,80 @@ namespace ConnectingToSql
         private void btnSave_Click(object sender, EventArgs e)
         {
             sqlcon.Open();
-            cmd = new SqlCommand("insert into student(stdName,stdClass,stdAdress,stdContact)values('"+txtName.Text+"','"+cmbClass.Text+"','"+txtAdress.Text+"','"+txtContact.Text+"')",sqlcon);
+            cmd = new SqlCommand("insert into student(stdName,stdClass,stdAdress,stdContact)values('" + txtName.Text + "','" + cmbClass.Text + "','" + txtAdress.Text + "','" + txtContact.Text + "')", sqlcon);
             cmd.ExecuteNonQuery();
             MessageBox.Show("RECORDS INSERTED GOOD JOB!");
             sqlcon.Close();
+
+        }
+
+        private void urlTxt(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_OkUrl(object sender, EventArgs e)
+        {
+            txtDataResp.Clear();
+            var url = txtUrlRaw.Text;
+
+            WebRequest request = HttpWebRequest.Create(url);
+
+            WebResponse response = request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string responseText = reader.ReadToEnd();
+
+            jsonObjectSave = JObject.Parse(responseText);
+
+            Console.WriteLine(responseText);
+            Console.WriteLine(" JSON ARRAY UNDER");
+            Console.WriteLine(jsonObjectSave);
+            textBox2_TextChanged(jsonObjectSave);
+            MessageBox.Show("Data succesfully fetched!");
+            txtUrlRaw.Clear();
+        }
+
+        private void textBox2_TextChanged(JObject jObject)
+        {
+            txtDataResp.Text = jObject.ToString();
+
+        }
+
+
+        private void btn_saveToSql3(object sender, EventArgs e)
+        {
+            if (jsonObjectSave == null)
+            {
+                MessageBox.Show("You need to first fetch the API Data");
+            }
+            else
+            {
+                Console.WriteLine("Inside save to SQL 3");
+                Console.WriteLine(jsonObjectSave.ToString());
+                string name = (string)jsonObjectSave.GetValue("name");
+                string age = (string)jsonObjectSave.GetValue("age");
+                string count = (string)jsonObjectSave.GetValue("count");
+                string contact = "1121337";
+                sqlcon.Open();
+                cmd = new SqlCommand("insert into student(stdName,stdClass,stdAdress,stdContact)values('" + name + "','" + age + "','" + count + "','" + contact + "')", sqlcon);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("RECORDS INSERTED GOOD JOB!");
+                sqlcon.Close();
+
+            }
+        }
+
+
+        private void btn_ConnectSql3(object sender, EventArgs e)
+        {
+            sqlcon = conSqlServ.GetConnection();
+            MessageBox.Show("Connect Succesfull!");
+        }
+
+        private void txtDataResp_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
